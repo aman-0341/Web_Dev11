51af66ac2d75acaedb0427ea34ff5d059acbd102
aman-0341 <amanbudhraja18@gmail.com>
2024-07-08 22:06:21 +0530

    We have handled submit case

--- React11/demoshopapp2/src/components/ProductForm.js
+++ React11/demoshopapp2/src/components/ProductForm.js
@@ -11,21 +11,37 @@ function ProductForm(props)
     }
     function dateChangeHandler(event)
     {
-        setTitle(event.target.value);
+        setDate(event.target.value);
         // console.log(event.target.value);
-        console.log(title);
-        console.log(date);
+        // console.log(title);
+        // console.log(date);
     }
-    return(<form>
+    function submitHandler(event)
+    {
+        event.preventDefault();
+
+        const productData={
+            title:title,
+            date:date
+        };
+        //Here we are just assigning this objects the name titlt:titlt confuse mat hona .
+        //Ye object ki entites ke name hai different bhi ho sakte hai
+
+        console.log(productData);
+
+        setTitle(' ');
+        setDate(' ');
+    }
+    return(<form onSubmit={submitHandler}>
         <div className='new-product_controls'>
         <label>Title</label>
-        <input type='text' onChange={titleChangeHandler}></input>
+        <input type='text' value={title}  onChange={titleChangeHandler} ></input>
 
 
         </div>
         <div className='new-product_control'>
             <label>Date</label>
-            <input type='date' min='01-01-2023' max='12-12-2023' onChange={dateChangeHandler}></input>
+            <input type='date' value={date} min='2023-01-01' max='2023-12-12'  onChange={dateChangeHandler} ></input>
         </div>
         <div className='new-product_button'>
             <button type="submit">Add Product</button>
